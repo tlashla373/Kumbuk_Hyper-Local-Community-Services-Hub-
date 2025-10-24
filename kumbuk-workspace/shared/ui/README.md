087db1bd7d01f6c94e668bac9631cf138dac1684
Sandeepa Hiripitiya <umeshsandeepa1@gmail.com>
2025-10-24 08:59:41 +0530

    initial

---
+++ kumbuk-workspace/shared/ui/README.md
@@ -0,0 +1,7 @@
+# @kumbuk/shared-ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @kumbuk/shared-ui` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ kumbuk-workspace/shared/ui/src/lib/ui.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+/* eslint-disable-next-line */
+export interface UiProps {}
+
+export function Ui(props: UiProps) {
+  return (
+    <View>
+      <Text>Welcome to ui!</Text>
+    </View>
+  );
+}
+
+export default Ui;
